5ac1342ffbf0f1f196a6aff88388b968fa53e18d
Aron <graph@livemail.tw>
2022-08-02 19:25:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-header('Content-type: text/plain');
 
 $json = file_get_contents('https://api.mozambiquehe.re/bridge?version=4&platform=PC&player=kazafxu&auth=b1c456b9557ad0a41aa5475155fe41db');
 $data = json_decode($json);
